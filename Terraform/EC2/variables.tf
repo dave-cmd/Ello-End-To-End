42519e35adfbc18fa069d9a625863ae17a714179
Kanjuru Wahome <kanjurus8@gmail.com>
2023-10-19 15:58:03 +0300

    Terrafom EC2 complete

---
+++ Terraform/EC2/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "instance_type" {}
+variable "key_name" {}
